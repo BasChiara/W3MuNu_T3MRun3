f2f20c66eb5d691f13dbccfe8ec6d3bcd42c845c
Chiara Basile <basilechiara@libero.it>
2024-03-15 18:38:57 +0100

    first commit

---
+++ Configuration/GenProduction/python/ppW3MuNu_fragment.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+import os
+externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
+    args = cms.vstring('/eos/user/c/cbasile/Tau3MuRun3/W3MuNu_SM_production/genproductions/bin/MadGraph5_aMCatNLO/ppW3MuNu_smfull_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(500),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),
+)
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13600.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
+                                    )
+    )
+)
+ProductionFilterSequence = cms.Sequence(generator)
